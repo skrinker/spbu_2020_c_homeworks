ddbef9c41bb898efd3de8e31b1fbc8ca7db48750
skrinker <guzel28sit@gmail.com>
2020-09-28 02:52:36 +0300

    user friendly

---
+++ homework_2/task_1.c
@@ -0,0 +1,30 @@
+#include "stdbool.h"
+#include "stdio.h"
+
+void rec(int idx, int sum, int last, int a[], int n)
+{
+  if (sum == n) {
+    for (int i = 0; i < idx; i++) {
+      if (i == idx - 1)
+        printf("%d", a[i]);
+      else
+        printf("%d + ", a[i]);
+    }
+    printf("\n");
+    return;
+  }
+  for (int i = last; i <= n - sum; i++) {
+    a[idx] = i;
+    rec(idx + 1, sum + i, i, a, n);
+  }
+}
+
+int main()
+{
+  int n;
+  printf("Введите число: \n");
+  scanf("%d", &n);
+  int a[n];
+  rec(0, 0, 1, a, n);
+  return 0;
+}
