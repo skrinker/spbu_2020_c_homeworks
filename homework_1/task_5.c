65346fab4c0f37b49735bec9860307e7339d0f85
skrinker <guzel28sit@gmail.com>
2020-09-29 21:31:56 +0300

    cmake-fix

--- homework_1/task_5.c
+++ homework_1/task_5.c
@@ -5,70 +5,70 @@
 
 void merge(int arr[], int l, int m, int r)
 {
-  int i, j, k;
-  int n1 = m - l + 1;
-  int n2 = r - m;
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
 
-  int L[n1], R[n2];
+    int L[n1], R[n2];
 
-  for (i = 0; i < n1; i++)
-    L[i] = arr[l + i];
-  for (j = 0; j < n2; j++)
-    R[j] = arr[m + 1 + j];
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
 
-  i = 0;
-  j = 0;
-  k = l;
+    i = 0;
+    j = 0;
+    k = l;
 
-  while (i < n1 && j < n2) {
-    if (L[i] <= R[j]) {
-      arr[k] = L[i];
-      i++;
-    } else {
-      arr[k] = R[j];
-      j++;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        } else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
     }
-    k++;
-  }
 
-  while (i < n1) {
-    arr[k] = L[i];
-    i++;
-    k++;
-  }
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
 
-  while (j < n2) {
-    arr[k] = R[j];
-    j++;
-    k++;
-  }
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
 }
 
 void mergeSort(int arr[], int l, int r)
 {
-  if (l < r) {
-    int m = l + (r - l) / 2;
-    mergeSort(arr, l, m);
-    mergeSort(arr, m + 1, r);
-    merge(arr, l, m, r);
-  }
+    if (l < r) {
+        int m = l + (r - l) / 2;
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+        merge(arr, l, m, r);
+    }
 }
 
 int findRepeatedMax(int arr[], int r)
 {
-  int i;
-  for (i = 0; i < r - 1; i++) {
-    if (arr[i] == arr[i + 1]) {
-      return arr[i];
+    int i;
+    for (i = 0; i < r - 1; i++) {
+        if (arr[i] == arr[i + 1]) {
+            return arr[i];
+        }
     }
-  }
-  return -1;
+    return -1;
 }
 
 int main()
 {
-  int arr[n] = { 12, 11, 13, 5, 6, 7, 12 };
-  mergeSort(arr, 0, n - 1);
-  printf("%d", findRepeatedMax(arr, n - 1));
-  return 0;
+    int arr[n] = { 12, 11, 13, 5, 6, 7, 12 };
+    mergeSort(arr, 0, n - 1);
+    printf("%d", findRepeatedMax(arr, n - 1));
+    return 0;
 }
