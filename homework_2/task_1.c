73c537e21fd0a55f670fb6704e8c35b9a7835fc0
skrinker <guzel28sit@gmail.com>
2020-09-29 21:35:38 +0300

    clang-format

--- homework_2/task_1.c
+++ homework_2/task_1.c
@@ -3,28 +3,28 @@
 
 void rec(int idx, int sum, int last, int a[], int n)
 {
-  if (sum == n) {
-    for (int i = 0; i < idx; i++) {
-      if (i == idx - 1)
-        printf("%d", a[i]);
-      else
-        printf("%d + ", a[i]);
+    if (sum == n) {
+        for (int i = 0; i < idx; i++) {
+            if (i == idx - 1)
+                printf("%d", a[i]);
+            else
+                printf("%d + ", a[i]);
+        }
+        printf("\n");
+        return;
     }
-    printf("\n");
-    return;
-  }
-  for (int i = last; i <= n - sum; i++) {
-    a[idx] = i;
-    rec(idx + 1, sum + i, i, a, n);
-  }
+    for (int i = last; i <= n - sum; i++) {
+        a[idx] = i;
+        rec(idx + 1, sum + i, i, a, n);
+    }
 }
 
 int main()
 {
-  int n;
-  printf("Введите число: \n");
-  scanf("%d", &n);
-  int a[n];
-  rec(0, 0, 1, a, n);
-  return 0;
+    int n;
+    printf("Введите число: \n");
+    scanf("%d", &n);
+    int a[n];
+    rec(0, 0, 1, a, n);
+    return 0;
 }
