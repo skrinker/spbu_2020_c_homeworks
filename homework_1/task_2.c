4963da50ba31dbb353ffef02fe1db5fae26cad8b
skrinker <guzel28sit@gmail.com>
2020-09-29 21:21:33 +0300

    clang-format

--- homework_1/task_2.c
+++ homework_1/task_2.c
@@ -11,7 +11,8 @@ void swap(int* array, int startPoint, int endPoint)
 }
 int main()
 {
-    int n = 2 int m = 3;
+    int n = 2;
+    int m = 3;
     int a[5] = { 1, 2, 3, 4, 5 };
     swap(a, m, n + m - 1);
     swap(a, 0, m - 1);
