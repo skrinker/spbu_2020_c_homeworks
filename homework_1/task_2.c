af72a574b3c92df8f87a9af5a96952b0278bd767
skrinker <guzel28sit@gmail.com>
2020-09-29 21:21:23 +0300

    clang-format

--- homework_1/task_2.c
+++ homework_1/task_2.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-void swap(int *array, int startPoint, int endPoint)
+void swap(int* array, int startPoint, int endPoint)
 {
     int i;
     for (i = 0; i < (endPoint - startPoint) / 2 + 1; i++) {
@@ -11,12 +11,10 @@ void swap(int *array, int startPoint, int endPoint)
 }
 int main()
 {
-    int n = 2;
-    int m = 3;
+    int n = 2 int m = 3;
     int a[5] = { 1, 2, 3, 4, 5 };
-    swap(a,  m, n + m - 1);
+    swap(a, m, n + m - 1);
     swap(a, 0, m - 1);
     swap(a, 0, n + m - 1);
     return 0;
 }
-
