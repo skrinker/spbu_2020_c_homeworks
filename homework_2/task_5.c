47f5388a8f85c5e764deed9a123a3964e60bc4e0
skrinker <guzel28sit@gmail.com>
2020-09-29 21:41:06 +0300

    clang-format

--- homework_2/task_5.c
+++ homework_2/task_5.c
@@ -1,14 +1,15 @@
 #include "stdio.h"
 
-unsigned long long factorialRec(int n) 
+unsigned long long factorialRec(int n)
 {
-    if ((n == 0) || (n == 1)) return 1;
+    if ((n == 0) || (n == 1))
+        return 1;
     unsigned long long factorial = 0;
     factorial = factorialRec(n - 1) * n;
 }
 
 unsigned long long factorialLoop(int n)
-{   
+{
     unsigned long long factorial = 1;
     for (int i = 2; i <= n; i++) {
         factorial *= i;
@@ -16,7 +17,7 @@ unsigned long long factorialLoop(int n)
     return factorial;
 }
 
-int main() 
+int main()
 {
     int n;
     printf("Enter a number (up to and including 20):  \n");
