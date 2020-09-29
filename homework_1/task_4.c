94c67793e3a8b4396e7f92b4c29881157c9fec20
skrinker <guzel28sit@gmail.com>
2020-09-29 21:30:12 +0300

    cmake-fix

--- homework_1/task_4.c
+++ homework_1/task_4.c
@@ -3,25 +3,26 @@
 
 bool isPrime(int a)
 {
-  if (a % 2 == 0) return false; 
-  int i;
-  bool isPrime = true;
-  for (i = 3; i < a / 2; i = i + 2) {
-    if (a % i == 0) {
-      isPrime = false;
-      break;
+    if (a % 2 == 0)
+        return false;
+    int i;
+    bool isPrime = true;
+    for (i = 3; i < a / 2; i = i + 2) {
+        if (a % i == 0) {
+            isPrime = false;
+            break;
+        }
     }
-  }
-  return isPrime;
+    return isPrime;
 }
 
 int main()
 {
-  int n = 10000;
-  int i;
-  for (i = 2; i <= n; i++) {
-    if (isPrime(i)) {
-      printf("%d\n", i);
+    int n = 10000;
+    int i;
+    for (i = 2; i <= n; i++) {
+        if (isPrime(i)) {
+            printf("%d\n", i);
+        }
     }
-  }
 }
