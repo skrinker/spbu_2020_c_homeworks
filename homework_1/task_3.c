e9fd524e4ab1510f0ad3a84371d81882207b368e
skrinker <guzel28sit@gmail.com>
2020-10-06 23:51:54 +0300

    add library and fix loops

--- homework_1/task_3.c
+++ homework_1/task_3.c
@@ -1,84 +1,83 @@
+#include "../library/commonUtils/numericOperations.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-char* getString()
-{
-    int len = 0;
-    int capacity = 1;
-    char* s = (char*)malloc(capacity * sizeof(char));
+#define primeNumber 13
 
-    char c = getchar();
-
-    while (c != '\n') {
-        s[(len)++] = c;
-
-        if (len >= capacity) {
-            capacity *= 2;
-            s = (char*)realloc(s, capacity * sizeof(char));
-        }
-
-        c = getchar();
-    }
-
-    s[len] = '\0';
-
-    return s;
-}
-
-int randomNumber(int a)
-{
-    return rand() % a;
-}
-
-int getCharNumber(char a)
-{
-    if (a == ' ') {
-        return 26;
-    }
-    return a - 'a' + 1;
-}
-
-int binPow(int a, int n)
+int binaryPow(int a, int n)
 {
     if (n == 0) {
         return 1;
     }
     if (n % 2 == 1) {
-        return binPow(a, n - 1) * a;
+        return binaryPow(a, n - 1) * a;
     } else {
-        int c = binPow(a, n / 2);
+        int c = binaryPow(a, n / 2);
         return c * c;
     }
 }
 
-int hash(int a, int b, char str[], int primeNumber)
+char* getString()
 {
-    int i;
+    int length = 0;
+    int capacity = 1;
+    char* string = (char*)malloc(capacity * sizeof(char));
+    char symbol = getchar();
+    while (symbol != '\n') {
+        string[(length)++] = symbol;
+        if (length >= capacity) {
+            capacity *= 2;
+            string = (char*)realloc(string, capacity * sizeof(char));
+        }
+        symbol = getchar();
+    }
+    string[length] = '\0';
+    return string;
+}
+
+int getRandomNumber(int number)
+{
+    return rand() % number;
+}
+
+int getCharNumber(char symbol)
+{
+    if (symbol == ' ')
+        return 26;
+    return symbol - 'a' + 1;
+}
+
+int getHash(int a, int b, char str[])
+{
     int result = 0;
-    for (i = a; i < b; i++) {
-        result += binPow(primeNumber, b - a - i - 1) * getCharNumber(str[i]);
+    for (int i = a; i < b; i++) {
+        result += binaryPow(primeNumber, b - a - i - 1) * getCharNumber(str[i]);
     }
     return result;
 }
 
-int search(char pattern[], char text[], int primeNumber)
+int getNextSubstringHash(int buffHash, int patternLength, char text[], int index)
 {
-    int i;
+    return primeNumber * buffHash - binaryPow(primeNumber, patternLength) * getHash(index, index + 1, text)
+        + getHash(index + patternLength, index + patternLength + 1, text);
+}
+
+int searchString(char pattern[], char text[])
+{
     int n = 0;
     int patternLength = strlen(pattern);
     int textLength = strlen(text);
-    int patternHash = hash(0, patternLength, pattern, primeNumber);
-    int buffHash = hash(0, patternLength, text, primeNumber);
-    for (i = 0; i < textLength - patternLength + 1; i++) {
+    int patternHash = getHash(0, patternLength, pattern);
+    int buffHash = getHash(0, patternLength, text);
+    for (int i = 0; i < textLength - patternLength + 1; i++) {
         if (buffHash == patternHash) {
-            int c = randomNumber(patternLength);
+            int c = getRandomNumber(patternLength);
             if (text[i + c] == pattern[c]) {
                 n++;
             }
         }
-        buffHash = primeNumber * buffHash - binPow(primeNumber, patternLength) * hash(i, i + 1, text, primeNumber)
-            + hash(i + patternLength, i + patternLength + 1, text, primeNumber);
+        buffHash = getNextSubstringHash(buffHash, patternLength, text, i);
     }
     return n;
 }
@@ -88,7 +87,7 @@ int main()
     printf("input text:\n");
     char* text = getString();
     printf("enter the string to find:\n");
-    char* searchString = getString();
-    printf("total occurrences: %d \n", search(searchString, text, 13));
+    char* searchStr = getString();
+    printf("total occurrences: %d \n", searchString(searchStr, text));
     return 0;
 }
--- library/commonUtils/numericOperations.c
+++ library/commonUtils/numericOperations.c
@@ -9,3 +9,15 @@ int max(int a, int b)
 {
     return a > b ? a : b;
 }
+
+int binaryPow(int number, int power)
+{
+    int result = 1;
+    while (number) {
+        if (number & 1)
+            result *= number;
+        number *= number;
+        power >>= 1;
+    }
+    return result;
+}
