83b2af25f035419f0acde97b5c4b83711f621fb5
skrinker <guzel28sit@gmail.com>
2020-10-07 00:13:49 +0300

    add free() & add initilisation

--- homework_2/task_1.c
+++ homework_2/task_1.c
@@ -1,11 +1,12 @@
 #include "stdbool.h"
 #include "stdio.h"
+#include "stdlib.h"
 
-void rec(int idx, int sum, int last, int a[], int n)
+void decompose(int index, int sum, int last, int a[], int number)
 {
-    if (sum == n) {
-        for (int i = 0; i < idx; i++) {
-            if (i == idx - 1)
+    if (sum == number) {
+        for (int i = 0; i < index; ++i) {
+            if (i == index - 1)
                 printf("%d", a[i]);
             else
                 printf("%d + ", a[i]);
@@ -13,18 +14,19 @@ void rec(int idx, int sum, int last, int a[], int n)
         printf("\n");
         return;
     }
-    for (int i = last; i <= n - sum; i++) {
-        a[idx] = i;
-        rec(idx + 1, sum + i, i, a, n);
+    for (int i = last; i <= number - sum; ++i) {
+        a[index] = i;
+        decompose(index + 1, sum + i, i, a, number);
     }
 }
 
 int main()
 {
-    int n;
+    int number = 0;
     printf("Введите число: \n");
-    scanf("%d", &n);
-    int a[n];
-    rec(0, 0, 1, a, n);
+    scanf("%d", &number);
+    int* array = (int*)malloc(number * sizeof(int));
+    decompose(0, 0, 1, array, number);
+    free(array);
     return 0;
 }
