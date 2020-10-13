1c74af7bd87e2dddb4828ee335cfa49549219e79
skrinker <guzel28sit@gmail.com>
2020-10-14 01:38:33 +0300

    fix

--- homework_2/task_3.c
+++ homework_2/task_3.c
@@ -1,7 +1,7 @@
 #include "stdio.h"
 #include "stdlib.h"
 
-void putZerosToEnd(int array[], int size)
+void putZerosToEnd(int* array, int size)
 {
     int count = 0;
 
@@ -23,6 +23,8 @@ int main()
 
     printf("Введите массив: \n");
     int* array = (int*)malloc(size * sizeof(int));
+    for (int i = 0; i < size; ++i)
+        array[i] = 0;
     for (int i = 0; i < size; ++i)
         scanf("%d", &array[i]);
 
