a008834c6092d391456289aba7569142972e68c7
skrinker <guzel28sit@gmail.com>
2020-10-17 16:22:52 +0300

    add +

--- homework_4/task_2.c
+++ homework_4/task_2.c
@@ -1,14 +1,25 @@
 #include "math.h"
 
+char getSign(double number)
+{
+    if (!signbit(number))
+        return '+';
+}
+
+void printExponentialForm(double number)
+{
+    double mantissa = 0;
+    int exponent = 0;
+    mantissa = frexp(number, &exponent);
+    printf("Result: ");
+    printf("%f = %c%.20f * 2^{%d}\n", number, getSign(number), mantissa * 2, exponent - 1);
+}
+
 int main()
 {
     double number = 0;
-    double mantissa = 0;
-    int exponent = 0;
     printf("Enter a number: ");
     scanf("%lf", &number);
-    mantissa = frexp(number, &exponent);
-    printf("Result: ");
-    printf("%f = %.20f * 2^{%d}\n", number, mantissa * 2, exponent - 1);
+    printExponentialForm(number);
     return 0;
 }
