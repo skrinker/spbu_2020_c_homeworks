f1753f23f8eaaf7eff5e5c1f54df8233886929df
skrinker <guzel28sit@gmail.com>
2020-10-21 05:11:11 +0300

    fix string, locate, deletelist, getnext, getvalue, getsize

--- homework_3/main.c
+++ homework_3/main.c
@@ -1,11 +1,29 @@
-#include "list.c"
+#include "../library/list/list.h"
+
+int checkPosition(int position, List* list)
+{
+    while (position >= getSize(list) || position < 0) {
+        printf("position less/more than necessary, try again \n");
+        scanf("%d", &position);
+    }
+    return position;
+}
+
+int checkPositionToInsert(int position, List* list)
+{
+    while (position > getSize(list) || position < 0) {
+        printf("position less/more than necessary, try again \n");
+        scanf("%d", &position);
+    }
+    return position;
+}
 
 int main()
 {
     int n = 0;
     printf("Input number of items: \n");
     scanf("%d", &n);
-    ListElement** elements = (ListElement**)malloc(n * sizeof(ListElement));
+    ListElement** elements = (ListElement**)malloc(n * sizeof(ListElement*));
     List* list = createList();
     for (int i = 0; i < n; i++) {
         int number = 0;
@@ -20,10 +38,7 @@ int main()
     printf("choose position: \n");
     int position = 0;
     scanf("%d", &position);
-    while (position > list->size) {
-        printf("position more than list length, try again \n");
-        scanf("%d", &position);
-    }
+    position = checkPositionToInsert(position, list);
     printf("input value: \n");
     int value = 0;
     scanf("%d", &value);
@@ -33,46 +48,52 @@ int main()
     printf("\n");
 
     printf("let's get head value \n");
-    printf("%d", head(list)->value);
+    printf("%d", getValue(head(list)));
     printf("\n");
 
     printf("let's get tail value \n");
-    printf("%d", tail(list)->value);
+    printf("%d", getValue(tail(list)));
     printf("\n");
 
     printf("let's retrieve list element value \n");
     printf("choose position: \n");
     scanf("%d", &position);
-    while (position >= list->size) {
-        printf("position more than list length, try again \n");
-        scanf("%d", &position);
-    }
-    printf("%d", retrieve(position, list)->value);
+    position = checkPosition(position, list);
+
+    printf("%d", getValue(retrieve(position, list)));
     printf("\n");
 
     printf("let's locate list element \n");
     scanf("%d", &value);
     ListElement* listElement2 = createListElement(value);
-    printf("%d", locate(listElement2, list));
-    printf("\n");
+    printf("%d \n", locate(listElement2, list));
+    ListElement* listElement3 = createListElementWithNext(value, retrieve(position + 1, list));
+    printf("%d \n", locate(listElement3, list));
 
     printf("let's delete list element \n");
     printf("choose position: \n");
     scanf("%d", &position);
-    while (position >= list->size) {
-        printf("position more than list length, try again \n");
-        scanf("%d", &position);
-    }
+    position = checkPosition(position, list);
+
     deleteElement(position, list);
     printList(list);
     printf("\n");
 
+    printf("are you sure you want to delete list [Y/n]?");
+    char symbol = "";
+    scanf("%s", &symbol);
+    if (symbol == 'Y' || symbol == 'y') {
+        deleteList(list);
+    }
+
     free(listElement1);
     free(listElement2);
+    free(listElement3);
+
     for (int i = 0; i < n; i++) {
         free(elements[i]);
     }
     free(elements);
-    free(list);
+
     return 0;
 }
---
+++ library/list/list.h
@@ -0,0 +1,28 @@
+#include "stdbool.h"
+#include "stdio.h"
+#include "stdlib.h"
+
+#ifndef SPBU_2020_C_LIST_H
+#define SPBU_2020_C_LIST_H
+
+typedef struct ListElement ListElement;
+
+typedef struct List List;
+
+List* createList();
+ListElement* getNext(ListElement* element);
+bool isEmpty(List* list);
+ListElement* createListElement(int value);
+ListElement* createListElementWithNext(int value, ListElement* next);
+ListElement* tail(List* list);
+ListElement* head(List* list);
+bool insert(ListElement* element, int position, List* list);
+int locate(ListElement* element, List* list);
+ListElement* retrieve(int position, List* list);
+bool deleteElement(int position, List* list);
+void printList(List* list);
+int getSize(List* list);
+int getValue(ListElement* element);
+void deleteList(List* list);
+
+#endif
