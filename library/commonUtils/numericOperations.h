572cd69440cd46b994a00a07d200bc8a0d89653e
skrinker <guzel28sit@gmail.com>
2020-10-25 21:59:08 +0300

    fix cmake

--- library/commonUtils/numericOperations.h
+++ library/commonUtils/numericOperations.h
@@ -7,5 +7,6 @@ int max(int a, int b);
 unsigned long long binaryPow(unsigned long long a, unsigned long long n);
 void splitDigits(unsigned long long number, int array[], int arrayLength);
 int getNumberLength(unsigned long long number);
+int compare(const void* x1, const void* x2);
 
 #endif //SPBU_2020_C_NUMERICOPERATIONS_H
