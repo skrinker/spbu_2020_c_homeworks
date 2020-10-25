868d8e7e1d048cf53fe690600a5bbb18263ecc6f
skrinker <guzel28sit@gmail.com>
2020-10-25 15:51:16 +0300

    fix

--- library/commonUtils/numericOperations.h
+++ library/commonUtils/numericOperations.h
@@ -4,8 +4,6 @@
 void swap(int* a, int* b);
 int max(int a, int b);
 int min(int a, int b);
-void splitDigits(int number, int array[], int arrayLength);
-int getNumberLength(int number);
 int binaryPow(int number, int power);
 
 #endif //SPBU_2020_C_NUMERICOPERATIONS_H
