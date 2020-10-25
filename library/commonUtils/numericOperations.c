88f292ff9e4adf0b079af1a7a2b247ce07739b2a
skrinker <guzel28sit@gmail.com>
2020-10-25 16:46:26 +0300

    fix cmake

--- library/commonUtils/numericOperations.c
+++ library/commonUtils/numericOperations.c
@@ -51,15 +51,3 @@ int getNumberLength(int number)
     }
     return numberLength;
 }
-
-int binaryPow(int a, int n)
-{
-    int res = 1;
-    while (n) {
-        if (n & 1)
-            res *= a;
-        a *= a;
-        n >>= 1;
-    }
-    return res;
-}
