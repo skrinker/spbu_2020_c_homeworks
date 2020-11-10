c98f4b4fdbf3808191ae3e936d33385cccdf768b
skrinker <guzel28sit@gmail.com>
2020-11-10 12:38:02 +0300

    leak memory

--- test_2/task_1.c
+++ test_2/task_1.c
@@ -24,10 +24,24 @@ int main()
     for (int i = 0; i < 2; i++) {
         result[i] = malloc(2 * sizeof(int));
     }
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            result[i][j] = 0;
+        }
+    }
     printf("Input number: \n");
     int number = 0;
     scanf("%d", &number);
     printf("Result: \n %d \n", getFibonachiNumber(matrix, result, firstMatrix, number));
+
+    for (int i = 0; i < 2; i++) {
+        free(matrix[i]);
+        free(result[i]);
+        free(firstMatrix[i]);
+    }
+    free(matrix);
+    free(result);
+    free(firstMatrix);
 
     return 0;
 }
