1b70683c653a206d45cd9ef66c6626177eca47a3
skrinker <guzel28sit@gmail.com>
2020-12-03 19:21:47 +0300

    hide

--- homework_8/task_1.c
+++ homework_8/task_1.c
@@ -1,6 +1,11 @@
+#include "../library/commonUtils/stringOperations.h"
 #include "../library/dfa/dfa.h"
+#include "stdio.h"
 
 int main()
 {
+    char* inputString = NULL;
+    inputString = getString();
+    printf("%s", inputString);
     return 0;
 }
